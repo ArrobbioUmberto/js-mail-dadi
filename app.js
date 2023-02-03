914cd1e8e767925c28fbbecbd00043e94aa802b7
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-02-03 17:28:41 +0100

    FEAT text on html

--- app.js
+++ app.js
@@ -7,11 +7,29 @@ console.log(mail)
 const emailUtente = prompt("Dimmi pure la tua mail")
 console.log(emailUtente)
 // controllare che la sua mail sia nella lista 
+const messaggeForUser = document.getElementById('mail')
+console.log(messaggeForUser)
 for (let i = 0; i < mail.length; i++){
+    let emailVerify = emailUtente
+
+    let emailDenied = emailUtente
+
+
     if (emailUtente === mail[i]){
-        console.log('la mail esiste')
-    }else {
-        console.log('la mail non si trova qua dentro')
+        messaggeForUser.innerHTML = '<h1> Ottimo ' + emailVerify + ' è presente nel nostro database. Bentornato </h1>'
+        break
+        // console.log('mail verificata')
+    } else {
+        messaggeForUser.innerHTML = '<h1> Peccato ' + emailDenied + ' non fa parte del nostro database.Iscriviti alla community se vuoi rimanere aggiornato </h1>'
     }
+
+
 }
+
+
+
+
+
+
+
 // se c'è digli che è una bella mail sennò digli che ti dispiace ma la mail non è presente
